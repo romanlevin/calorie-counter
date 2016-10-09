94c5e77ad4a7a0b1e7d3887d15f1d28cc403ae28
Roman Levin <romanlevin@gmail.com>
2016-10-09 15:10:12 +0200

    split admin view test

--- calorie_counter/tests/test_users.py
+++ calorie_counter/tests/test_users.py
@@ -1,4 +1,5 @@
 import pytest
+from calorie_counter.models import CalorieLimit
 
 
 @pytest.mark.django_db
@@ -26,6 +27,17 @@ def test_create_user_as_admin(client, django_user_model):
     new_user = django_user_model.objects.all()[1]
     assert new_user.username == 'admin'
     assert new_user.calorie_limit.calorie_limit == 100
-    response = client.get('/api/users/')
-    new_user = response.json()[1]
-    new_user['calorie_limit'] == 100
+
+
+@pytest.mark.django_db
+def test_view_user_as_admin(client, django_user_model):
+    user = django_user_model.objects.create_user(
+        username='user1',
+        password='magical',
+    )
+    CalorieLimit.objects.create(
+        user=user,
+        calorie_limit=100,
+    ).save()
+    response = client.get('/api/users/%d/' % user.pk)
+    assert response.json()['calorie_limit'] == 100
