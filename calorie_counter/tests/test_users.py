749639ec2f26e524833f314c5564c8f608197273
Roman Levin <romanlevin@gmail.com>
2016-10-09 15:09:38 +0200

    -assert expected number of users

--- calorie_counter/tests/test_users.py
+++ calorie_counter/tests/test_users.py
@@ -5,12 +5,14 @@ import pytest
 def test_list_users(client):
     response = client.get('/api/users/')
     assert response.status_code == 200
+    assert len(response.json()) == 1
 
 
 @pytest.mark.django_db
 def test_list_users_as_admin(client):
     response = client.get('/api/users/')
     assert response.status_code == 200
+    assert len(response.json()) == 1
 
 
 @pytest.mark.django_db
